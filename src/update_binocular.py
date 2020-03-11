c1349516ba8e25ef87cd8fb0d301e9c34d51b45d
adityakoolkarni <adityakulkarni1000@gmail.com>
2020-03-11 15:51:56 -0700

    Removal of points out of interest

--- src/update_binocular.py
+++ src/update_binocular.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.linalg import expm
 from tqdm import tqdm
+import time
 
 from utils import visualize_trajectory_2d,load_data
 
@@ -30,6 +31,7 @@ def imu_ekf(data_set):
     Binocular Camera
     '''
     time_stamp,features,v,omega,K,b,cam_T_imu = load_data(data_set)
+    start_time = time.time()
 
     z = features
     opt_T_imu = cam_T_imu
@@ -52,7 +54,7 @@ def imu_ekf(data_set):
                    @ expm(-tau * curly_hat(omega_hat,v[:,t])).T + W
 
     #visualize_trajectory_2d(pose_mean)
-    print("Done IMU Predict")
+    print("Done IMU Predict and time taken is ", time.time()-start_time)
     return pose_mean
 
 
@@ -93,17 +95,28 @@ def visual_ekf(pose_mean,z,k,b,cam_T_imu):
     '''
 
     print("Starting Mapping Update")
-    num_landmark = 200#z.shape[1]
+    start_time = time.time()
+    num_landmark = z.shape[1]
     landmark_mean = np.zeros((3*num_landmark)) # 3M
     #landmark_mean = np.zeros((3,num_landmark)) # 3,M
-    landmark_cov  = 1e-6 * np.eye(3*num_landmark) #3M x 3M
+    #landmark_cov  = 1e-3 * np.eye(3*num_landmark) #3M x 3M
+    landmark_cov  = np.diag(1e-6*np.random.randn(3*num_landmark))
 
     P_T = np.hstack((np.eye(3),np.zeros((3,1)))).T
     M = np.hstack((k[0:2,0:3],np.zeros((2,1))))
     M = np.vstack((M,M))
     M[2,3] = -k[0,0] * b #Disparity
-
     total_time = z.shape[2]
+
+    #for t in tqdm(range(total_time)):
+    #    jacobian = np.zeros((4*num_landmark, 3*num_landmark))
+    #    z_tik = np.zeros((4 * num_landmark))
+    #    z_sum = np.sum(z[:,0:num_landmark,t],axis=0)
+    #    valid_scans = np.where(z_sum != -4)
+    #    print(len(valid_scans[0]))
+
+    #exit()
+
     no_observation = np.array([-1,-1,-1,-1])
     first_observation = np.zeros(3)
     for t in tqdm(range(total_time)):
@@ -115,7 +128,6 @@ def visual_ekf(pose_mean,z,k,b,cam_T_imu):
         #for landmark in range(num_landmark-1):
         for landmark in valid_scans[0]:
             lnd_mrk_strt, lnd_mrk_end = landmark * 3, landmark * 3 + 3
-            stereo_strt, stereo_end = landmark * 4, landmark * 4 + 4
             if(np.all(landmark_mean[lnd_mrk_strt:lnd_mrk_end] == first_observation)):
                 #landmark_mean[lnd_mrk_strt:lnd_mrk_end] = inv_pi(np.linalg.inv(M.T @ M) @ M.T \
                 #                                              @ z[:,landmark,t].reshape(4,1))[0:3,0]
@@ -127,17 +139,30 @@ def visual_ekf(pose_mean,z,k,b,cam_T_imu):
                 landmark_mean_homo = np.vstack((landmark_mean[lnd_mrk_strt:lnd_mrk_end].reshape(3,1),1))
                 landmark_camera = pi(cam_T_imu @ pose_mean[:, :, t] @ landmark_mean_homo)
                 dpi_dq = deri_pi(landmark_camera)
-                jcbn_rstart,jcbn_rend = landmark*4,landmark*4 + 4 #Address
-                jcbn_cstart,jcbn_cend = landmark*3,landmark*3 + 3 #Address
-                z_tik[stereo_strt:stereo_end] = (M @ landmark_camera).flatten()
-                jacobian[jcbn_rstart:jcbn_rend,jcbn_cstart:jcbn_cend] = M @  dpi_dq @ cam_T_imu @ pose_mean[:,:,t] @ P_T
+                strt,end = landmark*3,landmark*3 + 3 #Address
+                z_tik = (M @ landmark_camera).flatten()
+                jacobian = M @  dpi_dq @ cam_T_imu @ pose_mean[:,:,t] @ P_T
+                k_gain = landmark_cov[strt:end,strt:end] @ jacobian.T @ \
+                         np.linalg.inv(jacobian @  landmark_cov[strt:end,strt:end] @ jacobian.T \
+                                       + np.diag(1e6*np.random.randn(4)))
+                landmark_mean[strt:end] = landmark_mean[strt:end] + k_gain @ (z[:,landmark,t] - z_tik)
+                landmark_cov[strt:end,strt:end] = (np.eye(3) - k_gain @ jacobian) @ landmark_cov[strt:end,strt:end]
+
 
 
         # update with Jacobian
-        i_x_v = np.kron(np.eye(num_landmark), np.random.randn(1)* np.eye(4))
-        k_gain = landmark_cov @ jacobian.T @ np.linalg.inv(jacobian @ landmark_cov @ jacobian.T + i_x_v) # TODO Noise
-        landmark_mean = landmark_mean + k_gain @ (z[:,0:num_landmark,t].flatten()- z_tik)
+        #TODO: This may not be required moved to loop above
+        #start_time = time.time()
+        #i_x_v = np.kron(np.eye(num_landmark), np.random.randn(1)* np.eye(4))
+        #print(time.time()-start_time)
+        #start_time = time.time()
+        #k_gain = landmark_cov @ jacobian.T @ np.linalg.inv(jacobian @ landmark_cov @ jacobian.T + i_x_v) # TODO Noise
+        #print(time.time()-start_time)
+        #start_time = time.time()
+        #landmark_mean = landmark_mean + k_gain @ (z[:,0:num_landmark,t].flatten()- z_tik)
+        #print(time.time()-start_time)
 
+    print("Done Mapping update and time taken is ", time.time()-start_time)
     visualize_trajectory_2d(pose_mean,landmark_mean.reshape(-1,3).T)
 
 
@@ -152,7 +177,6 @@ if __name__ == '__main__':
     print('m',features.shape)
     print('M',K.shape)
     print('M',K)
-    print('b',b.shape)
     visual_ekf(imu_ekf(data_set),features,K,b,cam_T_imu)
 
 
