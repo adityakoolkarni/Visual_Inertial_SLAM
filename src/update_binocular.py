85c6088cdc8043b1152dbc0d8901fa603683ce52
adityakoolkarni <adityakulkarni1000@gmail.com>
2020-03-10 18:17:47 -0700

    With Mapping Update

--- src/update_binocular.py
+++ src/update_binocular.py
@@ -29,6 +29,7 @@ def imu_ekf():
     Binocular Camera
     '''
     time_stamp,features,v,omega,K,b,cam_T_imu = load_data('data/0022.npz')
+
     z = features
     opt_T_imu = cam_T_imu
     prev_pose = np.eye(4)
@@ -50,7 +51,85 @@ def imu_ekf():
                    @ expm(-tau * curly_hat(omega_hat,v[:,t])).T + W
 
 
-    visualize_trajectory_2d(pose_mean)
+
+
+    #visualize_trajectory_2d(pose_mean)
+    return pose_mean
+
+
+def pi(point):
+    '''
+    :param : point in 3D
+    :return: projected point in 2D
+    '''
+    return point / point[2]
+
+def inv_pi(point):
+    '''
+
+    :param point: In 2D
+    :return: in 3D
+    '''
+    assert point.shape == (4,1)
+    return point * point[3]
+
+def deri_pi(point):
+    '''
+
+    :param point: derivative at this point is taken
+    :return: derivative
+    '''
+
+    return np.array([[1,0,-point[0]/point[2],0],
+                     [0,1,-point[1]/point[2],0],
+                     [0,0,1,0],
+                     [0,0,-point[3]/point[2],1]]) / point[2]
+
+def visual_ekf(pose_mean,z,k,b,cam_T_imu):
+    '''
+    :param pose_mean: The estimated pose for the IMU
+    :return:
+    '''
+
+    num_landmark = 2000#z.shape[1]
+    landmark_mean = np.zeros((3*num_landmark)) # 3M
+    #landmark_mean = np.zeros((3,num_landmark)) # 3,M
+    landmark_cov  = np.zeros((3*num_landmark,3*num_landmark)) #3M x 3M
+
+    P_T = np.hstack((np.eye(3),np.zeros((3,1)))).T
+    M = np.hstack((k[0:2,0:3],np.zeros((2,1))))
+    M = np.vstack((M,M))
+    M[2,3] = -k[0,0] * b #Disparity
+
+    total_time = z.shape[2]
+    no_observation = np.array([-1,-1,-1,-1])
+    first_observation = np.zeros(3)
+    for t in range(total_time):
+        jacobian = np.zeros((4*num_landmark, 3*num_landmark))
+        z_tik = np.zeros((3 * num_landmark))
+        for landmark in range(num_landmark-1):
+            lnd_mrk_strt, lnd_mrk_end = landmark * 3, landmark * 3 + 3
+            if(np.all(z[:,landmark,t] == no_observation)):
+                pass
+            elif(np.all(landmark_mean[lnd_mrk_strt:lnd_mrk_end] == first_observation)):
+                landmark_mean[lnd_mrk_strt:lnd_mrk_end] = inv_pi(np.linalg.inv(M.T @ M) @ M.T \
+                                                              @ z[:,landmark,t].reshape(4,1))[0:3,0]
+                #initialize
+            else:
+                landmark_mean_homo = np.vstack((landmark_mean[lnd_mrk_strt:lnd_mrk_end].reshape(3,1),1))
+                landmark_camera = pi(cam_T_imu @ pose_mean[:, :, t] @ landmark_mean_homo)
+                dpi_dq = deri_pi(landmark_camera)
+                jcbn_rstart,jcbn_rend = landmark*4,landmark*4 + 4 #Address
+                jcbn_cstart,jcbn_cend = landmark*3,landmark*3 + 3 #Address
+                z_tik[lnd_mrk_strt:lnd_mrk_end] = M @ landmark_camera
+                jacobian[jcbn_rstart:jcbn_rend,jcbn_cstart:jcbn_cend] = M @  dpi_dq @ cam_T_imu @ pose_mean[:,:,t] @ P_T
+
+        # update with Jacobian
+        k_gain = landmark_cov @ jacobian.T @ (jacobian @ landmark_cov @ jacobian.T) # TODO Noise
+        landmark_mean = landmark_mean + k_gain @ (z[:,:,t].T.reshape(-1,1) - z_tik)
+
+    visualize_trajectory_2d(pose_mean,landmark_mean.reshape(-1,3).T)
+
 
 
 if __name__ == '__main__':
@@ -60,6 +139,6 @@ if __name__ == '__main__':
     print('M',K.shape)
     print('M',K)
     print('b',b.shape)
-    imu_ekf()
+    visual_ekf(imu_ekf(),features,K,b,cam_T_imu)
 
 
--- src/utils.py
+++ src/utils.py
@@ -42,7 +42,7 @@ def load_data(file_name):
   return t,features,linear_velocity,rotational_velocity,K,b,cam_T_imu
 
 
-def visualize_trajectory_2d(pose,path_name="Unknown",show_ori=False):
+def visualize_trajectory_2d(pose,landmark_mean,path_name="Unknown",show_ori=False):
   '''
   function to visualize the trajectory in 2D
   Input:
@@ -55,6 +55,8 @@ def visualize_trajectory_2d(pose,path_name="Unknown",show_ori=False):
   ax.plot(pose[0,3,:],pose[1,3,:],'r-',label=path_name)
   ax.scatter(pose[0,3,0],pose[1,3,0],marker='s',label="start")
   ax.scatter(pose[0,3,-1],pose[1,3,-1],marker='o',label="end")
+  ax.scatter(landmark_mean[0,:],landmark_mean[1,:])
+
   if show_ori:
       select_ori_index = list(range(0,n_pose,max(int(n_pose/50), 1)))
       yaw_list = []
